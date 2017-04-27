3968224b602b73e0f37369a94965e7f19a15a7c6
Michael Koceja <michael.koceja@gmail.com>
2017-04-27 09:55:45 -0700

    fix type in id img2, made each image clickable

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(function() {
+    $("#img").click(function() {
+      alert("Animal info.");
+  });
+    $("#img2").click(function() {
+      alert("Animal info.");
+  });
+    $("#img3").click(function() {
+      alert("animal info.");
+  });
+});
